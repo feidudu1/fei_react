d0574455cf6984f943dfe537d386224b04e3787a
yafei <tianbian5@163.com>
2019-08-13 19:06:47 +0800

    【umi_shujuzichan】header

--- umi_shujuzichan/src/layouts/index.js
+++ umi_shujuzichan/src/layouts/index.js
@@ -1,10 +1,55 @@
-import styles from './index.less';
+import Link from 'umi/link'
+import style from './index.less';
 
+const screenSwitchInfo = [{
+    route: '/s3',
+    name: '数据概览',
+  },
+  {
+    route: '/s4',
+    name: '数据链路',
+  },
+  {
+    route: '/',
+    name: '资产评估',
+  },
+  {
+    route: '/',
+    name: '资产地图',
+  },
+];
 function BasicLayout(props) {
+  const activeItem = props.data || '数据概览'
+  console.log(11, activeItem);
+  
   return (
-    <div className={styles.normal}>
-      <h1 className={styles.title}>Yay! Welcome to umi!</h1>
-      {props.children}
+    <div className={style.bigScreenTop}>
+      <div className={style.screenTitle}></div>
+      <div className={style.screenLink}>
+        {
+          screenSwitchInfo.map(screenInfo => (
+            <button
+              key={screenInfo.name}
+              className = {
+                `${style.linkBtn} ${screenInfo.name === activeItem ? style.linkActive :''}`
+              }
+              >
+              <Link
+                className={style.link}
+                to={screenInfo.route}
+                disabled={screenInfo.name === activeItem ? 'disabled' : false}
+                >{screenInfo.name}
+              </Link>
+            </button>
+          ))
+        }
+      </div>
+      <div className={style.screenRightLink}>
+        <div className={style.rightBg1}></div>
+        <div className={style.rightBg2}></div>
+        <div className={style.rightBg3}></div>
+        <a className={style.screenRightLinkTitle} href="http://www.baidu.com">资产管理</a>
+      </div>
     </div>
   );
 }
