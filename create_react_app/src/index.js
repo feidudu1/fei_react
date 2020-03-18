7294f3acd2ab679343530eb80417944bc59c8c6e
yafei <tianbian5@163.com>
2020-03-18 12:41:19 +0800

    【create_react_app】配置路由

--- create_react_app/src/index.js
+++ create_react_app/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './page/App/App';
+import Nav from './page/Nav/index';
+import Home from './page/Home/index';
+import PureComponent from './page/PureComponent/index';
 import './index.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router>
+    <div>
+      <Nav />
+    </div>
+    <Switch>
+      <Route exact path="/"><Home/></Route> // 这里必须加上exact属性，不然会覆盖掉/app的路由，即/app显示的还是/的页面
+      <Route path="/app"><App/></Route>
+      <Route path="/pureComponent"><PureComponent/></Route>
+    </Switch>
+  </Router>
+),document.getElementById('root'));
+
+// ReactDOM.render(<App/>,document.getElementById('root'));
---
+++ create_react_app/src/page/Home/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './index.css';
+// import NavLink from './navLink'
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="App">
+        welcome home
+      </div>
+    );
+  }
+}
+
+export default Home;
